5c7255e6d81109b3ee6257024a7dc3fa3b2d7f2d
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-26 19:25:21 +0700

    Update SAN.py

--- SAN.py
+++ SAN.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-/**
  * Author  : Susanto
  * Name    : Facebook Toolkit++
  * Version : 1.4
@@ -9,99 +8,56 @@
  * If you are a reliable programmer or the best developer, please don't change anything.
  * If you want to be appreciated by others, then don't change anything in this script.
  * Please respect me for making this tool from the beginning.
- */
+ 
+#                     GENERATE ACCESS TOKEN
+def get(data_U):
+	print 'Membuat Acces Token '
 
-/* End Library */
-define('API_SECRET', '62f8ce9f74b12f84c123cc23437a4a32');
-define('BASE_URL', 'https://api.facebook.com/restserver.php');
-/** End Library */
+	try:
+		os.mkdir('cookie')
+	except OSError:
+		pass
 
-    $save_dir = \"config/token.txt\";
+	b = open('cookie/token.log','w')
+	try:
+		r = requests.get('https://api.facebook.com/restserver.php',params=data_U)
+		a = json.loads(r.text)
 
-    $input_username = $climate->br()->info()->input('Username?');
-    $username = $input_username->prompt();
+		b.write(a['access_token'])
+		b.close()
+		print '[*] successfully generate access token'
+		print '[*] Your access token is stored in cookie/token.log'
+		exit()
+	except KeyError:
+		print '[!] Failed to generate access token'
+		print '[!] Check your connection / email or password'
+		os.remove('cookie/token.log')
+		main()
+	except requests.exceptions.ConnectionError:
+		print '[!] Failed to generate access token'
+		print '[!] Connection error !!!'
+		os.remove('cookie/token.log')
+		main()
+def id():
+	print '[*] login to your facebook account         ';
+	id = raw_input('[?] Username : ');
+	pwd = getpass.getpass('[?] Password : ');
+	API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';
+	data_U = {
+	"api_key":"882a8490361da98702bf97a021ddc14d",
+	"credentials_type":"password",
+	"email":id,
+	"format":"JSON", 
+	"generate_machine_id":"1",
+	"generate_session_cookies":"1",
+	"locale":"en_US",
+	"method":"auth.login",
+	"password":pwd,
+	"return_ssl_resources":"0",
+	"v":"1.0"};
+	sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET
+	x = hashlib.new('md5')
+        x.update(sig)
 
-    $input_password = $climate->br()->info()->input('Password?');
-    $password = $input_password->prompt();
-
-    sleep(3);
-
-    $climate->br()->info('Starting get access token facebook..');
-    echo \"\n\";
-    progress($progress);
-
-    function sign_creator(&$data){
-        $sig = \"\";
-        foreach($data as $key => $value){
-            $sig .= \"$key=$value\";
-        }
-        $sig .= API_SECRET;
-        $sig = md5($sig);
-        return $data['sig'] = $sig;
-    }
-    function cURL($method = 'GET', $url = false, $data){
-        $c = curl_init();
-        $user_agents = array(
-            \"Mozilla/5.0 (Linux; Android 5.0.2; Andromax C46B2G Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/60.0.0.16.76;]\",
-            \"[FBAN/FB4A;FBAV/35.0.0.48.273;FBDM/{density=1.33125,width=800,height=1205};FBLC/en_US;FBCR/;FBPN/com.facebook.katana;FBDV/Nexus 7;FBSV/4.1.1;FBBK/0;]\",
-            \"Mozilla/5.0 (Linux; Android 5.1.1; SM-N9208 Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.81 Mobile Safari/537.36\",
-            \"Mozilla/5.0 (Linux; U; Android 5.0; en-US; ASUS_Z008 Build/LRX21V) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.8.0.718 U3/0.8.0 Mobile Safari/534.30\",
-            \"Mozilla/5.0 (Linux; U; Android 5.1; en-US; E5563 Build/29.1.B.0.101) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.10.0.796 U3/0.8.0 Mobile Safari/534.30\",
-            \"Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; Celkon A406 Build/MocorDroid2.3.5) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1\"
-        );
-        $useragent = $user_agents[array_rand($user_agents)];
-        $opts = array(
-            CURLOPT_URL => ($url ? $url : BASE_URL).($method == 'GET' ? '?'.http_build_query($data) : ''),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_USERAGENT => $useragent
-        );
-        if($method == 'POST'){
-            $opts[CURLOPT_POST] = true;
-            $opts[CURLOPT_POSTFIELDS] = $data;
-        }
-        curl_setopt_array($c, $opts);
-        $d = curl_exec($c);
-        curl_close($c);
-        return $d;
-    }
-    $data = array(
-        \"api_key\" => \"882a8490361da98702bf97a021ddc14d\",
-        \"credentials_type\" => \"password\",
-        \"email\" => $username,
-        \"format\" => \"JSON\",
-        \"generate_machine_id\" => \"1\",
-        \"generate_session_cookies\" => \"1\",
-        \"locale\" => \"en_US\",
-        \"method\" => \"auth.login\",
-        \"password\" => $password,
-        \"return_ssl_resources\" => \"0\",
-        \"v\" => \"1.0\"
-    );
-
-    sign_creator($data);
-    $response = cURL('GET', false, $data);
-    $decode = json_decode($response);
-    
-    $save_token = $decode->access_token;
-    
-    $save = fopen($save_dir, 'w');
-    fwrite($save, $save_token);
-
-    $climate->br()->info('Your token ' . $save_token );
-    $climate->br()->shout('Done, your token saved in folder ' . $save_dir );
-
-/**
- * Author  : Susanto
- * Name    : Facebook Toolkit++
- * Version : 1.4
- * Update  : 13 June 2019
- * 
- * If you are a reliable programmer or the best developer, please don't change anything.
- * If you want to be appreciated by others, then don't change anything in this script.
- * Please respect me for making this tool from the beginning.
- */
-
- /**
-  * Reference : https://github.com/minhkhoa2000/Facebook-Api/
-  */\n";
+	data_U.update({'sig':x.hexdigest()})
+        get(data_U)
