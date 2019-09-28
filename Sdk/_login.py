940b99f412e470694412f92db8cfbcffeda69d26
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-29 00:40:24 +0700

    Create _login.py

---
+++ Sdk/_login.py
@@ -0,0 +1,27 @@
+from selenium import webdriver 
+from time import sleep 
+  
+usr=input('Enter Email Id:')  
+pwd=input('Enter Password:')  
+  
+driver = webdriver.Chrome() 
+driver.get('https://graph.facebook.com/oauth/access_token?client_id=6628568379&client_secret=c1e620fa708a1d5696fb991c1bde5662&grant_type=client_credentials') 
+print ("Opened facebook") 
+sleep(1) 
+  
+username_box = driver.find_element_by_id('email') 
+username_box.send_keys(usr) 
+print ("Email Id entered") 
+sleep(1) 
+  
+password_box = driver.find_element_by_id('pass') 
+password_box.send_keys(pwd) 
+print ("Password entered") 
+  
+login_box = driver.find_element_by_id('loginbutton') 
+login_box.click() 
+  
+print ("Done") 
+input('Press anything to quit') 
+driver.quit() 
+print("Finished"
