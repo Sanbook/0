4925b2295e38c4aa981756f37ff3dbe249dbf7f6
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-29 10:33:33 +0700

    Update SAN.py

--- SAN.py
+++ SAN.py
@@ -219,13 +219,29 @@ def getdata():
 	main()
 
 def id():
-	print '[*] login to your facebook account         ';id = raw_input('[?] Username : ');pwd = getpass.getpass('[?] Password : ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET
-	x = hashlib.new('md5')
+	print '[*] login to your facebook account         ';
+	id = raw_input('[?] Username : ');
+	pwd = getpass.getpass('[?] Password : ');
+	API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';
+	params = {"url":"https://graph.facebook.com/oauth/access_token%0A%20%20%20","raw_url":"https://graph.facebook.com/oauth/access_token\n   ?client_id={6628568379}\n   &client_secret={c1e620fa708a1d5696fb991c1bde5662}\n   &grant_type=client_credentials\n   &method=auth.login\n   &credentials_type=email='+id+'\n   &password=pwd\n   &format=JSON\n   &generate_machine_id=1\n   &generate_session_cookies=1\n   &locale=en_US\n   &return_ssl_resources=0\n   &v=1.0+c1e620fa708a1d5696fb991c1bde5662","method":"get","queries":{"client_id":"{6628568379}\n   ","client_secret":"{c1e620fa708a1d5696fb991c1bde5662}\n   ","grant_type":"client_credentials\n   ","method":"auth.login\n   ","credentials_type":"email=' id '\n   ","password":"pwd\n   ","format":"JSON\n   ","generate_machine_id":"1\n   ","generate_session_cookies":"1\n   ","locale":"en_US\n   ","return_ssl_resources":"0\n   ","v":"1.0 c1e620fa708a1d5696fb991c1bde5662"}}
+	sig = (
+    ('client_id', '{6628568379}\n   '),
+    ('client_secret', '{c1e620fa708a1d5696fb991c1bde5662}\n   '),
+    ('grant_type', 'client_credentials\n   '),
+    ('method', 'auth.login\n   '),
+    ('credentials_type', 'email=\' id \'\n   '),
+    ('password', 'pwd\n   '),
+    ('format', 'JSON\n   '),
+    ('generate_machine_id', '1\n   '),
+    ('generate_session_cookies', '1\n   '),
+    ('locale', 'en_US\n   '),
+    ('return_ssl_resources', '0\n   '),
+    ('v'),
+)	x = hashlib.new('md5')
         x.update(sig)
 
 	data.update({'sig':x.hexdigest()})
         get(data)
-
 
 
 ####################################################################
