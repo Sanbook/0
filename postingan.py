c4072b7696149fa7bcd360f5189ff57ef2fd4c61
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-02 13:32:33 +0700

    Update postingan.py

--- postingan.py
+++ postingan.py
@@ -7,15 +7,23 @@ sys . setdefaultencoding ( 'utf8' )
 
 
 def main():
-	main = input("Inputkan nilaimu: ")
-
-if main >= 'LOGIN':
-	login()
-elif main >= 'TOKEN':
-	token()
-elif main >= 'UNFRIENDS':
-	unfriend()
-		
+	i=input('ok>>> ')
+	if i=='':
+		print("[!] Wrong input")
+		time.sleep(1)
+		main()
+	elif i=='1' or i=='01':
+		login()
+	elif i=='2' or i=='02':
+		token()
+	elif i=='3' or i=='03':
+		unfriend()
+	elif i=='0' or i=='00':
+		os.sys.exit()
+	else:
+		print(m+'[!] Wrong input')
+		time.sleep(1)
+		main()
 
 def login():
 	print ("[*] login to your facebook account         ");id = raw_input("[?] Username : ");pwd = getpass.getpass("[?] Password : ");API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET
