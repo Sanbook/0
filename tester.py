ddfca88be3081d29a6487f31b8447e4780a8f1cc
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-03 04:11:15 +0700

    Update tester.py

--- tester.py
+++ tester.py
@@ -238,7 +238,6 @@ def group():
 		r = requests.get('https://graph.facebook.com/me?access_token=' + token)
 		a = json.loads(r.text)
 		for i in a['data']:
-		os.system('clear')
 		print('Mencoba mengambil nomor refrensi group dari' +i[name])
 		print('mohon tunggu sebentar, program masih mengumpulkan refrensi group.')
 		r = requests.post('https://graph.facebook.com/me/groups?access_token='+token)
