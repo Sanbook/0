21d2493722f2744fba512bcd6f11b4c80f5e7072
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-02 12:23:54 +0700

    Update _mbusek_postingan.py

--- Sdk/_mbusek_postingan.py
+++ Sdk/_mbusek_postingan.py
@@ -30,11 +30,6 @@ def main():
 		unfriend()
 		main()
 		
-  except KeyboardInterrupt:
-	main()
-  except IndexError:
-	print '[!] invalid parameter on command : ' + cek
-	main()
 
 def login():
 	print '[*] login to your facebook account         ';id = raw_input('[?] Username : ');pwd = getpass.getpass('[?] Password : ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET
