50a550e36f95d1f22c10e05fa092ff7977cb3324
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-04 10:54:06 +0700

    Update new.py

--- new.py
+++ new.py
@@ -19,7 +19,7 @@ def login_api():
 
 		API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';params = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":email,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":password,"return_ssl_resources":"0","v":"1.0"};response = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+email+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+password+'return_ssl_resources=0v=1.0'+API_SECRET
 		x = hashlib.new('md5')
-        x.update(response)
+        	x.update(response)
 
 		params.update({'response':x.hexdigest()})
 		
